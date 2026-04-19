5af4dbeb05f288932dff38ae1ad9380882d687b5
Karthikkk07 <karthikdreddy501@gmail.com>
2026-04-19 08:52:43 +0530

    Add auto-migrations to wsgi.py

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -16,3 +16,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
 app = application
 
+# Run migrations automatically on startup (Serverless friendly)
+from django.core.management import call_command
+try:
+    print("Running migrations...")
+    call_command('migrate', interactive=False)
+    print("Migrations complete.")
+except Exception as e:
+    print(f"Migration error: {e}")
+
